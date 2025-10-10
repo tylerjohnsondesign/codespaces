59935cee1f769eece3e30c3afab53f02857f2020
Tyler Johnson <tyler@builtmighty.com>
2025-10-10 06:05:11 +0000

    (main) 🚧 WIP on html.php

--- html.php
+++ html.php
@@ -8,27 +8,36 @@
     <body>
         <h1 id="main-heading" class="main" style="background:red">Hello!</h1>
         <p class="main">My name is Tyler.</p>
-        <div class="main">
-            <p style="color:turquoise">Hello, this is a paragraph.</p>
+        <div class="main" style="height:100vh">
+            <a href="https://youtube.com"><p style="color:turquoise">Hello, this is a paragraph.</p></a>
             <a href="https://youtube.com" target="_blank">YouTube</a>
-            <a href="#main-heading">This is the heading.</a><?php 
-
-                // Check if set.
-                if( isset( $_POST ) && ! empty( $_POST ) ) {
-
-                    // Output. ?>
-                    <p>Thanks for subscribing <?php echo $_POST['email']; ?>!</p><?php
-
-                } ?>
-
+            <table>
+                <thead>
+                    <tr>
+                        <th>Hobby</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Ride Horses</td>
+                        <td>Elizabeth</td>
+                    </tr>
+                    <tr>
+                        <td>Play Video Games</td>
+                        <td>Timothy</td>
+                    </tr>
+                </tbody>
+            </table>
             <form method="POST">
                 <p>
-                    <label for="email">Email</label>
-                    <input name="email" type="email" placeholder="Your email address..." />
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" placeholder="Enter your email..." />
                 </p>
                 <p>
-                    <label for="gender">Gender</label>
+                    <label for="gender">Gender:</label>
                     <select name="gender">
+                        <option value="">--Please choose an option--</option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
                     </select>
@@ -38,27 +47,5 @@
                 </p>
             </form>
         </div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Hobby</th>
-                    <th>About</th>
-                </tr>    
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Brazilian Jiu-Jitsu</td>
-                    <td>Martial art and combat sport. Something about it.</td>
-                </tr>
-                <tr>
-                    <td>Gaming</td>
-                    <td>Playing video games and exploring virtual worlds.</td>
-                </tr>
-                <tr>
-                    <td>Coding</td>
-                    <td>Creating software and applications to solve problems.</td>
-                </tr>
-            </tbody>
-        </table>
     </body>
 </html>
