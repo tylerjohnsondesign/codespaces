0f74730587418e0e3c3042455a87867745da59ce
Tyler Johnson <tyler@builtmighty.com>
2025-10-10 06:05:22 +0000

    (main) ✨ YouTube starting templates

---
+++ youtube.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>YouTube</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/assets/youtube.css" media="all" />
+    </head>
+    <body>
+        <!-- Get started here -->
+        <!-- https://picsum.photos/530/300 -->
+    </body>
+</html>
