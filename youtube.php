4e929d9c71880df9ed1103ca5e1d6066baa40d59
Tyler Johnson <tyler@builtmighty.com>
2025-10-24 14:03:27 +0000

    Youtube Updates

--- youtube.php
+++ youtube.php
@@ -7,21 +7,34 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="/assets/youtube.css" media="all" />
+        <link rel="stylesheet" href="/assets/youtube.css?ver=<?php echo date('YmdHis'); ?>" media="all" />
     </head>
     <body>
         <!-- Get started here -->
         <div id="header">
             <div class="header-inner header-left">
+                <button id="hamburger">
+                    <?php include dirname(__FILE__) . '/assets/hamburger.svg'; ?>
+                </button>
                 <img id="logo" src="/assets/youtube.svg" />
             </div>
             <div class="header-inner header-center">
-                <form method="POST" id="search">
-                    <input type="text" name="search" placeholder="Search" />
+                <form method="GET" id="search" action="https://www.youtube.com/results">
+                    <input type="text" name="search_query" placeholder="Search" />
                     <input type="submit" value="Search" />
                 </form>
+                <button id="voice-search">
+                    <img src="/assets/microphone.svg" />
+                </button>
             </div>
             <div class="header-inner header-right">
+                <button id="settings">
+                    <img src="/assets/vertical-dots.svg" />
+                </button>
+                <button id="user">
+                    <img src="/assets/user.svg" />
+                    <span>Sign In</span>
+                </button>
             </div>
         </div>
         <!-- https://picsum.photos/530/300 -->
